f5790648dcfaf0eddca396267b00a0ea42b85b83
Fusman Aleksandr <AF250329@ncr.com>
2024-03-07 13:25:38 +0200

    added actual command implementation

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -25,7 +26,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("running")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
